12a75876413331a55139d7d12e8d3b210cc4a08a
Kaiwinta <131403480+Kaiwinta@users.noreply.github.com>
2024-12-02 12:39:07 +0100

    [Feat]: Ajout part 2 et refactor

--- Day02/code.py
+++ Day02/code.py
@@ -1,41 +1,42 @@
 def part1():
-    safe_nb = 0
-    reports = []
+    safe_count = 0
     with open("Day02/Input.txt") as src:
         reports = src.readlines()
+
     for report in reports:
-        report = report.split(' ')
-        report = [int(value) for value in report]
-        sorted = (all(report[i] < report[i+1] for i in range(len(report) - 1))
-                 or all(report[i] > report[i+1] for i in range(len(report) - 1)))
-        if sorted == False:
-            continue
-        level_differed = (all(report[i] - report[i+1] <= 3 and report[i] - report[i+1] > 0 for i in range(len(report) - 1))
-                        or all(report[i + 1] - report[i] <= 3 and report[i + 1] - report[i] > 0 for i in range(len(report) - 1)))
-        if level_differed == False:
-            continue
-        safe_nb += 1
-    print(safe_nb)
+        report = list(map(int, report.split()))
+        if is_safe(report):
+            safe_count += 1
+    print(safe_count)
+
+def is_safe(report):
+    for i in range(len(report) - 1):
+        diff = abs(report[i] - report[i + 1])
+        if diff < 1 or diff > 3:
+            return False
+    increasing = all(report[i] < report[i + 1] for i in range(len(report) - 1))
+    decreasing = all(report[i] > report[i + 1] for i in range(len(report) - 1))
+    return increasing or decreasing
+
+def can_be_safe_with_removal(report):
+    for i in range(len(report)):
+        # Delete the actual node to check if it's safe without
+        modified_report = report[:i] + report[i + 1:]
+        if is_safe(modified_report):
+            return True
+    return False
 
 def part2():
-    safe_nb = 0
-    reports = []
-    comparator = 0
+    safe_count = 0
     with open("Day02/Input.txt") as src:
         reports = src.readlines()
+
     for report in reports:
-        report = report.split(' ')
-        report = [int(value) for value in report]
-        superior = 0
-        inferior = 0
-        equals = 0
-        for i in range(len(report)):
-            superior += report[i] > report[i + 1]
-            inferior += report[i] < report[i + 1]
-            equals   += report[i] == report[i + 1]
-        # Too much invalids datas
-        if (superior > 1 and (inferior > 1 or equals > 1) or (inferior > 1 and equals > 1)):
-            continue
+        report = list(map(int, report.split()))
+        if is_safe(report) or can_be_safe_with_removal(report):
+            safe_count += 1
+    print(safe_count)
+
 
 
 if __name__ == "__main__":
