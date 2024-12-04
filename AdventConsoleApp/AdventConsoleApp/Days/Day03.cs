7561d70c9836ca131b15644e7cd87f8abebf3122
Kaiwinta <131403480+Kaiwinta@users.noreply.github.com>
2024-12-04 15:45:01 +0100

    [Feat]: Add day03 C#

--- AdventConsoleApp/AdventConsoleApp/Days/Day03.cs
+++ AdventConsoleApp/AdventConsoleApp/Days/Day03.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AdventConsoleApp.Days
 {
     public class Day03 : BaseDay
     {
+        private int GetProductOfString(string data)
+        {
+            int total = 0;
+            foreach (var match in Regex.EnumerateMatches(data, @"mul\([0-9]{1,3},[0-9]{1,3}\)"))
+            {
+                string mul = data.Substring(match.Index, match.Length);
+
+                // Remove the mul(
+                int index = mul.IndexOf('(') + 1;
+                // Split the string to get only the numbers
+                mul = mul.Substring(index, mul.Length - index - 1);
+                var values = mul.Split(',');
+
+                total += int.Parse(values[0]) * int.Parse(values[1]);
+            }
+            return total;
+        }
+
         protected override int Part1()
         {
-            return 2;
+            return GetProductOfString(Data);
+        }
+
+        protected override int Part2()
+        {
+            int total = 0;
+
+            foreach (var match in Regex.EnumerateMatches(Data, @"(do\(\)((?!don't\(\)).)*mul\([0-9]+,[0-9]+\))"))
+            {
+                // Search all the mul in a substring that contains only enabled mul
+                total += GetProductOfString(Data.Substring(match.Index, match.Length));
+            }
+            foreach (var match in Regex.EnumerateMatches(Data, @"^(((?!(don't\(\)|do\(\))).)*mul\([0-9]+,[0-9]+\))"))
+            {
+                // Search all the mul in a substring that contains only enabled mul
+                total += GetProductOfString(Data.Substring(match.Index, match.Length));
+            }
+            return total;
         }
     }
 }
