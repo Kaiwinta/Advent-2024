c42a6023192919b08858ff25365b38506f4c1a7d
Kaiwinta <131403480+Kaiwinta@users.noreply.github.com>
2024-12-05 17:13:07 +0100

    Update Program.fs

--- Day02/F#/Program.fs
+++ Day02/F#/Program.fs
@@ -6,15 +6,14 @@ open System.IO
 let lines = File.ReadAllLines("Input.txt")
 let lineList = Seq.toList(lines);
 
+let ToInt str : int = str |> int
+
 let isSafe(line :string) :bool = 
     let values = line.Split(" ")
     let compare = 0
     let lastValue = -1
-    values |> List.iter (fun value ->
-        let intValue = value |> int
+    values |> List.map ToInt |>
         if lastValue == -1 then (lastValue = intValue)
-        else ()
-            
     )
     values.Length > 0
 
