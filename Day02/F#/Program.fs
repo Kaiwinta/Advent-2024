74a86eed6f59807068ffc4cc251f107654add718
Kaiwinta <131403480+Kaiwinta@users.noreply.github.com>
2024-12-06 17:04:09 +0100

    Update Program.fs

--- Day02/F#/Program.fs
+++ Day02/F#/Program.fs
@@ -1,26 +1,61 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-
-open System.IO
-
-let lines = File.ReadAllLines("Input.txt")
-let lineList : List<string> = Seq.toList(lines);
+﻿open System.IO
 
 let ToInt str : int = str |> int
 
-let isSafe(line :string) :bool = 
-    let values = line.Split(" ")
-    let compare = 0
-    let lastValue = -1
-    values |> List.map ToInt |>
-        if lastValue = -1 then (lastValue <- intValue)
-        else ()
-    values.Length > 0
+let ToListInt(line :string) : list<int> = 
+    let values = line.Split(' ')
+    values |> List.ofArray |> List.map ToInt
 
-lineList |> List.iter (fun line ->
-    printfn "%s" line
-    printfn "%b" (isSafe line)
-)
+let Abs(value :int) :int=
+    let mutable possitiveValue = value
+    if value < 0 then
+        possitiveValue <- value * -1
+    possitiveValue
+
+let removeAt(index :int, list) =
+    // Récupération des i premier element puis skip des i + 1 premier
+    List.take index list @ List.skip (index + 1) list
+
+let IsSafe (vals: list<int>) : bool =
+    let mutable safe = true
+    for i in 1 .. vals.Length - 2 do
+        if (vals.[i] > vals.[i + 1] && (vals.[i - 1] > vals.[i]) <> true) then
+            safe <- false
+        elif (vals.[i] < vals.[i + 1] && (vals.[i - 1] < vals.[i]) <> true) then
+            safe <- false
+    if (safe) then
+        for i in 0 .. vals.Length - 2 do
+            if (Abs(vals.[i] - vals.[i + 1]) < 1 || Abs(vals.[i] - vals.[i + 1]) > 3) then
+                safe <-false
+    safe
+
+let IsSafeWithRemoval(intVals :list<int>) :bool =
+    for i in 0 .. intVals.Length do
+        let tempList = removeAt i intVals
+        if (IsSafe tempList) then
+            true
+    false
 
 
-let test = 4
+let Part1 (lines :list<string>) :int =
+    let mutable total = 0
+    for line in lines do
+        let intVals = ToListInt line
+        if (IsSafe intVals) then
+            total <- total + 1
+    total
+
+let Part2 (lines :list<string>) :int =
+    let mutable total = 0
+    for line in lines do
+        let intVals = ToListInt line
+        if (IsSafe intVals) then
+            total <- total + 1
+        elif (IsSafeWithRemoval intVals) then
+            total <- total + 1
+    total
+
+let lines = File.ReadAllLines("Input.txt")
+let lineList : list<string> = Seq.toList(lines);
+
+printfn "%d" (Part1 lineList)
