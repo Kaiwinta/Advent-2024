e97718f98f3e8e4646fa47425e42a4fce6d0ff4a
Kaiwinta <pradouratitouan@proton.me>
2024-12-17 11:21:29 +0100

    Ajout part 2

--- Day11/code.py
+++ Day11/code.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def parse_input():
     value = ""
     with open ("Day11/Input.txt") as src:
@@ -28,12 +30,28 @@ def part1(values):
     return len(values)
 
 def part2(values):
+    stone_values = defaultdict(int)
+    for value in values:
+        stone_values[value] += 1
     for i in range(75):
-        values = blink(values)
-
-    return len(values)
-
+        for index, amount in list(stone_values.items()):
+            if amount == 0:
+                continue
+            if index == 0:
+                stone_values[index] -= 1
+                stone_values[i] += 1
+                continue
+            str_value = str(index)
+            len_value = len(str_value)
+            if len_value % 2 == 0:
+                stone_values[int(str_value[:int(len_value / 2)])] += 1
+                stone_values[int(str_value[int(len_value / 2):])] += 1
+            else:
+                stone_values[index * 2024] += 1
+                stone_values[index] -= 1
+    return sum(stone_values.values())
+    
 if __name__ == "__main__":
     data = parse_input()
-    print(part1(data))
-    part2(data)
+    #print(part1(data))
+    print(part2(data))
